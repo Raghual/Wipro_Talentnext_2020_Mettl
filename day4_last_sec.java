99986b98384ed76be3742d06b49cb898782740ff
Raghual R <raviraghual@gmail.com>
2020-06-05 12:45:58 +0530

    Create day4_last_sec.java

---
+++ day4_last_sec.java
@@ -0,0 +1,18 @@
+package myproject;
+
+public class Last {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=1234;
+		int a = 0;
+		if(n/10==0) {
+			System.out.println(-1);
+		}
+		else
+			a=n/10;
+		System.out.println(a%10);
+	}
+}
+		
+
