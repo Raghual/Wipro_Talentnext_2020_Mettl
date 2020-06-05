79bbb048355a9726f50434edb020b2a6c4df57ff
Raghual R <raviraghual@gmail.com>
2020-06-05 12:32:51 +0530

    Create day4_Lastnumber.java

---
+++ day4_Lastnumber.java
@@ -0,0 +1,11 @@
+package myproject;
+
+public class LastDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=1234;
+		System.out.println(n%10);
+	}
+
+}
