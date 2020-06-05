d32e1258d2558ca8127b7dd717e3543e4795f287
Raghual R <raviraghual@gmail.com>
2020-06-05 12:55:23 +0530

    Create day5_isExactMultiple.java

---
+++ day5_isExactMultiple.java
@@ -0,0 +1,20 @@
+package myproject;
+
+public class ExactMultiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=10;
+		if(a==0 || b==0) {
+			System.out.println(3);
+		}
+		if(a%b==0) {
+		   System.out.println(2);
+		}
+		else
+			System.out.println(1);
+
+	}
+
+}
